2a8d536621badf48a840204332e68be58e3ca570
akdemirsafak <akdemirsafak@gmail.com>
2023-09-14 22:12:11 +0300

    Redis Message Broker feature example.

---
+++ Redis.Pub.Sub_PublisherExam/Redis.Pub.Sub_PublisherExam/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionMultiplexer connectionMultiplexer= await ConnectionMultiplexer.ConnectAsync("localhost:1234");
+ISubscriber subscriber= connectionMultiplexer.GetSubscriber();
+
+
+while (true)
+{
+    Console.Write("Message : ");
+    string message=Console.ReadLine();
+    await subscriber.PublishAsync("channelName", message);
+}
---
+++ Redis.Pub.Sub_SubscriberExam/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionMultiplexer connectionMultiplexer= await ConnectionMultiplexer.ConnectAsync("localhost:1234");
+ISubscriber subscriber= connectionMultiplexer.GetSubscriber();
+
+
+string message=Console.ReadLine();
+await subscriber.SubscribeAsync("channelName", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+Console.Read();
