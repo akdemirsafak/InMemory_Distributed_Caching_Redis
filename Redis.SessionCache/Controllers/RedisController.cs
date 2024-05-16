b08f614fe253ce4996ab77965066ea410e8c3c3c
akdemirsafak <akdemirsafak@gmail.com>
2024-05-16 23:07:09 +0300

    Redis Session Example Added.

---
+++ Redis.SessionCache/Controllers/RedisController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Redis.SessionCache.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RedisController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult SetSession()
+    {
+        HttpContext.Session.Set("name", Encoding.UTF8.GetBytes("Şafak AKDEMİR"));
+        return Ok("Session set.");
+    }
+    [HttpGet]
+    public IActionResult GetSession()
+    {
+        if (HttpContext.Session.TryGetValue("name", out byte[] value))
+            Console.WriteLine(Encoding.UTF8.GetString(value));
+
+        return Ok(Encoding.UTF8.GetString(value));
+    }
+}
---
+++ Redis.SessionCache/Program.cs
@@ -0,0 +1,46 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+    options.InstanceName = "RedisSession";
+});
+
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    //options.Cookie.HttpOnly = true;
+    //options.Cookie.IsEssential = true;
+});
+
+//Bu konfigürasyonlardan sonra session'lar direkt Redis sunucusunda saklanır.Böylece uygulamanın ölmesi durumunda bile session'lar kaybolmaz
+//ve uygulama daha az bellek kullanarak performans kaybına uğramaz.
+
+
+var app = builder.Build();
+
+app.UseSession();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
